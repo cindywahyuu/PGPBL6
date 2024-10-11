38e596d9f73ae21ec23abed09d3ebdbd8132ce4f
cindywahyuu <cindywahyukarisma@mail.ugm.ac.id>
2024-10-11 15:49:40 +0700

    update code marker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,16 @@ export class HomePage {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Fix for missing marker icons when deployed
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    });
+  }
 
   ionViewDidEnter(){
     // Inisialisasi peta
@@ -32,8 +41,6 @@ export class HomePage {
     const cartoDBPositron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.carto.com/">CartoDB</a>'
     });
-
-
 
     // Menambahkan layer control untuk memilih basemap
     const baseMaps = {
